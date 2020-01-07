35fcda863f75653bfb7a39575de0b45c83cc6d32
zach? <zpolleck@gmail.com>
2020-01-07 11:50:08 -0500

    ios port

---
+++ ios/MathOff/MathOff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathOff
+//
+//  Created by Zach on 2019/12/19.
+//  Copyright © 2019 Sanderson App Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
